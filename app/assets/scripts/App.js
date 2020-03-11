16ffe9ca9b72ef92c77180019d28633b984170c2
sawankumar <sawankumar.developer@gmail.com>
2020-03-11 15:43:39 +0530

    Everything fimish

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css'
+import 'lazysizes'
 import MobileMenu from './modules/mobilemenu'
 import StickyHeader from './modules/stickyheader'
 let stickyheader = new StickyHeader();
